70239802644eb5428fce41e3ffc949f8c78b9ae5
numtostr <vikasraj1911@gmail.com>
2020-11-16 18:12:08 +0530

    use vim.api.nvim_buf_delete() for deleting the buffer

--- lua/BufOnly.lua
+++ lua/BufOnly.lua
@@ -10,24 +10,19 @@ function M.BufOnly()
 
     local cur = api.nvim_get_current_buf()
 
-    local deleted = 0
-    local modified = 0
+    local deleted, modified = 0, 0
 
-    -- I think there is a bug in nvim_list_bufs
-    -- It doesn't update when bdelete is called
     for _, n in ipairs(api.nvim_list_bufs()) do
-        -- To mitigate the above issue I have to check if buffer is loaded or not
-        if api.nvim_buf_is_loaded(n) then
-            -- If the iter buffer is modified one, then don't do anything
-            if option(n, 'modified') then
-                modified = modified + 1
+        -- If the iter buffer is modified one, then don't do anything
+        if option(n, 'modified') then
+            modified = modified + 1
 
-            -- if iter is not equal to current buffer and it is modifiable then delete it
-            -- `modifiable` check is needed as it will prevent closing file tree ie. NERD_tree
-            elseif n ~= cur and (option(n, 'modifiable') or del_non_modifiable) then
-                cmd('bdelete '..n)
-                deleted = deleted + 1
-            end
+        -- iter is not equal to current buffer
+        -- iter is modifiable or del_non_modifiable == true
+        -- `modifiable` check is needed as it will prevent closing file tree ie. NERD_tree
+        elseif n ~= cur and (option(n, 'modifiable') or del_non_modifiable) then
+            api.nvim_buf_delete(n, {})
+            deleted = deleted + 1
         end
     end
 
